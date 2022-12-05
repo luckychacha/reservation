0502e82c5072195e7a996ff528bac33be7945925
luckychacha <358140284@qq.com>
2022-12-05 11:23:33 +0800

    day 4: create db and tables.

--- reservation/src/lib.rs
+++ reservation/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// cargo add sqlx --features chrono,uuid -p luckychacha-reservation
+// cargo add sqlx --features runtime-tokio-rustls,postgres -p luckychacha-reservation
+// cargo add sqlx -p luckychacha-reservation
+// cargo install sqlx-cli
+// sqlx migrate add init -r
+// touch .env
+// DATABASE_URL = 'postgres://username@host:port/reservation'
+// rm /usr/local/var/postgresql@11/postmaster.pid
+// brew services restart postgresql
+// sqlx migrate run
