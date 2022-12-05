8654e41b63d63cd9edf23eea2b33f3d4b240afe8
luckychacha <358140284@qq.com>
2022-12-05 18:20:06 +0800

    day 5: create db

--- reservation/src/lib.rs
+++ reservation/src/lib.rs
@@ -1,10 +1,18 @@
+// 1.add dependency
 // cargo add sqlx --features chrono,uuid -p luckychacha-reservation
 // cargo add sqlx --features runtime-tokio-rustls,postgres -p luckychacha-reservation
 // cargo add sqlx -p luckychacha-reservation
+
+// 2.install sqlx-cli
 // cargo install sqlx-cli
 // sqlx migrate add init -r
+
+// 3.init pgconfig
 // touch .env
 // DATABASE_URL = 'postgres://username@host:port/reservation'
+
 // rm /usr/local/var/postgresql@11/postmaster.pid
 // brew services restart postgresql
+
+// 4.run xxx.up.sql
 // sqlx migrate run
