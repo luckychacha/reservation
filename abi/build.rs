4f0c05323f39c140212920390d3d5f9983e19b72
luckychacha <358140284@qq.com>
2022-12-09 00:49:03 +0800

    day 8: finish add reservation.

--- abi/build.rs
+++ abi/build.rs
@@ -3,9 +3,9 @@ use std::process::Command;
 fn main() {
     tonic_build::configure()
         .out_dir("src/pb")
+        .type_attribute("reservation.ReservationStatus", "#[derive(sqlx::Type)]")
         .compile(&["protos/reservation.proto"], &["protos"])
         .unwrap();
-
     Command::new("cargo").args(["fmt"]).output().unwrap();
 
     println!("cargo:rerun-if-changed=protos/reservation.proto");
--- reservation/src/manager.rs
+++ reservation/src/manager.rs
@@ -1,10 +1,9 @@
 use crate::{ReservationError, ReservationId, ReservationManager, Rsvp};
 use async_trait::async_trait;
-use chrono::{DateTime, NaiveDate, NaiveDateTime, Utc};
+use chrono::{DateTime, Utc};
 use luckychacha_reservation_abi::{convert_to_utc_time, Reservation, ReservationQuery};
 use sqlx::postgres::types::PgRange;
 use sqlx::Row;
-use std::time::SystemTime;
 
 #[async_trait]
 impl Rsvp for ReservationManager {
@@ -13,21 +12,13 @@ impl Rsvp for ReservationManager {
             return Err(ReservationError::InvalidTime);
         }
 
-        let status = luckychacha_reservation_abi::ReservationStatus::from_i32(rsvp.status)
-            .unwrap_or(luckychacha_reservation_abi::ReservationStatus::Pending);
-        let start = convert_to_utc_time(rsvp.start.clone().unwrap());
-        let end = convert_to_utc_time(rsvp.end.clone().unwrap());
+        // let status = luckychacha_reservation_abi::ReservationStatus::from_i32(rsvp.status)
+        //     .unwrap_or(luckychacha_reservation_abi::ReservationStatus::Pending);
+        let start = convert_to_utc_time(rsvp.start.as_ref().unwrap().clone());
+        let end = convert_to_utc_time(rsvp.end.as_ref().unwrap().clone());
 
         let timespan: PgRange<DateTime<Utc>> = (start..end).into();
 
-        // let range: PgRange<> = (rsvp.start..rsvp.end).into();
-        // generate a insert sql for reservation.
-
-        // rsvp.user_id,
-        //             rsvp.resource_id,
-        //             timespan,
-        //             rsvp.note,
-        //             rsvp.status
         let id = sqlx::query(
             "INSERT INTO reservation(id, user_id, resource_id, start_time, end_time, note) VALUES (&1, $2, $3, $4) RETURNING id",
         )
@@ -35,7 +26,7 @@ impl Rsvp for ReservationManager {
             .bind(rsvp.resource_id.clone())
             .bind(timespan)
             .bind(rsvp.note.clone())
-            // .bind(luckychacha_reservation_abi::ReservationStatus::Pending)
+            .bind(rsvp.status)
             .fetch_one(&self.pool)
             .await?
             .get(0);
@@ -45,23 +36,23 @@ impl Rsvp for ReservationManager {
         Ok(rsvp)
     }
 
-    async fn change_status(&self, id: ReservationId) -> Result<Reservation, ReservationError> {
+    async fn change_status(&self, _id: ReservationId) -> Result<Reservation, ReservationError> {
         todo!()
     }
 
     async fn update_note(
         &self,
-        id: ReservationId,
-        note: String,
+        _id: ReservationId,
+        _note: String,
     ) -> Result<Reservation, ReservationError> {
         todo!()
     }
 
-    async fn delete(&self, id: ReservationId) -> Result<(), ReservationError> {
+    async fn delete(&self, _id: ReservationId) -> Result<(), ReservationError> {
         todo!()
     }
 
-    async fn get(&self, query: ReservationQuery) -> Result<Vec<Reservation>, ReservationError> {
+    async fn get(&self, _query: ReservationQuery) -> Result<Vec<Reservation>, ReservationError> {
         todo!()
     }
 }
