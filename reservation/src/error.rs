41befd512ca9fb7fd012d873fd5e483ccdeb37fa
luckychacha <358140284@qq.com>
2022-12-09 00:51:57 +0800

    day 8: finish add reservation.

--- reservation/src/error.rs
+++ reservation/src/error.rs
@@ -14,9 +14,9 @@ pub enum ReservationError {
     #[error("Database error")]
     DbError(#[from] sqlx::Error),
 
-    #[error("Invalid start or end time for reservation")]
+    #[error("Invalid start or end time for the reservation")]
     InvalidTime,
 
-    #[error("unknown data store error")]
+    #[error("unknown error")]
     Unknown,
 }
