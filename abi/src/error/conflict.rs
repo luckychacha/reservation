c4426dbc0f56bea610ee09565408bd04ec0257d7
luckychacha <358140284@qq.com>
2023-01-11 18:27:25 +0800

    day 15: rewrite parse sqlx conflict error info into struct.

--- abi/src/error/conflict.rs
+++ abi/src/error/conflict.rs
@@ -1,110 +1,100 @@
-use chrono::{DateTime, Utc};
-use regex::Regex;
 use std::{collections::HashMap, convert::Infallible, str::FromStr};
 
-#[derive(Debug, Clone)]
+use chrono::{DateTime, Utc};
+use regex::Regex;
+
+#[derive(Debug)]
 pub enum ReservationConflictInfo {
     Parsed(ReservationConflict),
     Unparsed(String),
 }
 
-impl FromStr for ReservationConflictInfo {
-    type Err = Infallible;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // s.parse() -> impl FromStr for ReservationConflict
-        if let Ok(conflict) = s.parse() {
-            Ok(ReservationConflictInfo::Parsed(conflict))
-        } else {
-            Ok(ReservationConflictInfo::Unparsed(s.to_string()))
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct ReservationConflict {
     pub new: ReservationWindow,
     pub old: ReservationWindow,
 }
 
-impl FromStr for ReservationConflict {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // from_str: impl FromStr for ParsedInfo，将字符串转为 ParsedInfo，即两个 HashMap
-        // try_into: impl TryFrom<ParsedInfo> for ReservationConflict，将 ParsedInfo 尝试转换为 ReservationConflict
-        ParsedInfo::from_str(s)?.try_into()
-    }
-}
-
-impl TryFrom<ParsedInfo> for ReservationConflict {
-    type Error = ();
-
-    // value.new.try_into()?: impl TryFrom<HashMap<String, String>> for ReservationWindow， 将 HashMap 转为 ReservationWindow
-    fn try_from(value: ParsedInfo) -> Result<Self, Self::Error> {
-        Ok(Self {
-            new: value.new.try_into()?,
-            old: value.old.try_into()?,
-        })
-    }
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct ReservationWindow {
     pub rid: String,
     pub start: DateTime<Utc>,
     pub end: DateTime<Utc>,
 }
 
-impl TryFrom<HashMap<String, String>> for ReservationWindow {
-    type Error = ();
+#[derive(Debug)]
+pub struct ParsedInfo {
+    pub new: HashMap<String, String>,
+    pub old: HashMap<String, String>,
+}
 
-    fn try_from(value: HashMap<String, String>) -> Result<Self, Self::Error> {
-        let timespan_str = value.get("timespan").ok_or(())?.replace('"', "");
-        let mut split = timespan_str.splitn(2, ',');
-        let start = parse_datetime(split.next().ok_or(())?)?;
-        let end = parse_datetime(split.next().ok_or(())?)?;
+impl FromStr for ReservationConflictInfo {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok(reservation_conflict) = s.parse() {
+            return Ok(ReservationConflictInfo::Parsed(reservation_conflict));
+        }
+        Ok(ReservationConflictInfo::Unparsed(s.to_string()))
+    }
+}
+
+impl FromStr for ReservationConflict {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parsed_info = ParsedInfo::from_str(s)?;
         Ok(Self {
-            rid: value.get("resource_id").ok_or(())?.to_string(),
-            start,
-            end,
+            new: parsed_info.new.try_into()?,
+            old: parsed_info.old.try_into()?,
         })
     }
 }
 
-fn parse_datetime(s: &str) -> Result<DateTime<Utc>, ()> {
+impl TryFrom<HashMap<String, String>> for ReservationWindow {
+    type Error = ();
+
+    fn try_from(value: HashMap<String, String>) -> Result<Self, Self::Error> {
+        // "2022-12-25 07:00:00+00","2022-12-28 03:00:00+00"
+        let timespan = value.get("timespan").ok_or(())?.replace('"', "");
+        let mut split = timespan.splitn(2, ',');
+        let start = split.next().ok_or(())?;
+        let end = split.next().ok_or(())?;
+
+        Ok(Self {
+            rid: value.get("resource_id").ok_or(())?.to_string(),
+            start: parse_str_into_datetime_utc(start)?,
+            end: parse_str_into_datetime_utc(end)?,
+        })
+    }
+}
+
+fn parse_str_into_datetime_utc(s: &str) -> Result<DateTime<Utc>, ()> {
     Ok(DateTime::parse_from_str(s, "%Y-%m-%d %H:%M:%S%#z")
         .map_err(|_| ())?
         .with_timezone(&Utc))
 }
 
-struct ParsedInfo {
-    new: HashMap<String, String>,
-    old: HashMap<String, String>,
-}
-
 impl FromStr for ParsedInfo {
     type Err = ();
 
-    // Key (resource_id, timespan)=(ocean-view-room-666, [\"2022-12-25 07:00:00+00\",\"2022-12-28 03:00:00+00\")) conflicts with existing key (resource_id, timespan)=(ocean-view-room-666, [\"2022-12-25 07:00:00+00\",\"2022-12-28 03:00:00+00\"))."
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // use regular expression
-        let re = Regex::new(
-            r#"\((?P<k1>[a-zA-Z0-9-_]+),\s*(?P<k2>[a-zA-Z0-9-_]+)\)=\((?P<v1>[a-zA-Z0-9-_]+),\s*\[(?P<v2>[^\)\]]+)"#,
-        ).unwrap();
+        let re = Regex::new(r"\((?P<k1>[a-zA-Z0-9-_]+),\s*(?P<k2>[a-zA-Z0-9-_]+)\)=\((?P<v1>[a-zA-Z0-9-_]+),\s*\[(?P<v2>[^\]\)]+)").unwrap();
+
         let mut maps = vec![];
-        for cap in re.captures_iter(s) {
-            let mut map = HashMap::new();
-            map.insert(cap["k1"].to_string(), cap["v1"].to_string());
-            map.insert(cap["k2"].to_string(), cap["v2"].to_string());
-            maps.push(Some(map));
+        for caps in re.captures_iter(s) {
+            let mut tmp = HashMap::new();
+            tmp.insert(caps["k1"].to_string(), caps["v1"].to_string());
+            tmp.insert(caps["k2"].to_string(), caps["v2"].to_string());
+
+            maps.push(Some(tmp));
         }
 
         if maps.len() != 2 {
             return Err(());
         }
 
-        Ok(ParsedInfo {
+        Ok(Self {
             new: maps[0].take().unwrap(),
             old: maps[1].take().unwrap(),
         })
@@ -119,7 +109,7 @@ mod tests {
 
     #[test]
     fn parsed_datetime_should_work() {
-        let dt = parse_datetime("2022-12-26 22:00:00+00").unwrap();
+        let dt = parse_str_into_datetime_utc("2022-12-26 22:00:00+00").unwrap();
         println!("dt: {}", dt);
         assert_eq!(dt.to_rfc3339(), "2022-12-26T22:00:00+00:00");
     }
