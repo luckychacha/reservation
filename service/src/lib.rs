3ea862e99486b2cdd33dbc8da6b577597c120cda
luckychacha <358140284@qq.com>
2023-02-08 13:58:28 +0800

    day 30: Add test on Service module.

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,91 +1,14 @@
 use std::pin::Pin;
 
 use futures::Stream;
-use luckychacha_reservation::{ReservationManager, Rsvp};
-use luckychacha_reservation_abi::{
-    reservation_service_server::ReservationService, CancelRequest, CancelResponse, Config,
-    ConfirmRequest, ConfirmResponse, FilterRequest, FilterResponse, GetRequest, GetResponse,
-    ListenRequest, QueryRequest, Reservation, ReserveRequest, ReserveResponse, UpdateRequest,
-    UpdateResponse,
-};
-use tonic::{async_trait, Request, Response, Status};
+use luckychacha_reservation::ReservationManager;
+use luckychacha_reservation_abi::Reservation;
+use tonic::Status;
+
+mod service;
+
 pub struct RsvpService {
     manager: ReservationManager,
 }
 
-impl RsvpService {
-    pub async fn from_config(config: &Config) -> Result<Self, anyhow::Error> {
-        Ok(Self {
-            manager: ReservationManager::from_config(&config.db).await?,
-        })
-    }
-}
-
 type ReservationStream = Pin<Box<dyn Stream<Item = Result<Reservation, Status>> + Send>>;
-#[async_trait]
-impl ReservationService for RsvpService {
-    async fn reserve(
-        &self,
-        request: Request<ReserveRequest>,
-    ) -> Result<Response<ReserveResponse>, Status> {
-        let request = request.into_inner();
-        if request.reservation.is_none() {
-            return Err(Status::invalid_argument("missing reservation"));
-        }
-        let reservation = self.manager.reserve(request.reservation.unwrap()).await?;
-
-        Ok(Response::new(ReserveResponse {
-            reservation: Some(reservation),
-        }))
-    }
-
-    async fn confirm(
-        &self,
-        _request: Request<ConfirmRequest>,
-    ) -> Result<Response<ConfirmResponse>, Status> {
-        todo!()
-    }
-
-    async fn update(
-        &self,
-        _request: Request<UpdateRequest>,
-    ) -> Result<Response<UpdateResponse>, Status> {
-        todo!()
-    }
-
-    async fn cancel(
-        &self,
-        _request: Request<CancelRequest>,
-    ) -> Result<Response<CancelResponse>, Status> {
-        todo!()
-    }
-
-    async fn get(&self, _request: Request<GetRequest>) -> Result<Response<GetResponse>, Status> {
-        todo!()
-    }
-
-    type queryStream = ReservationStream;
-
-    async fn query(
-        &self,
-        _request: Request<QueryRequest>,
-    ) -> Result<Response<Self::queryStream>, Status> {
-        todo!()
-    }
-
-    async fn filter(
-        &self,
-        _request: Request<FilterRequest>,
-    ) -> Result<Response<FilterResponse>, Status> {
-        todo!()
-    }
-
-    type listenStream = ReservationStream;
-
-    async fn listen(
-        &self,
-        _request: Request<ListenRequest>,
-    ) -> Result<Response<Self::listenStream>, Status> {
-        todo!()
-    }
-}
