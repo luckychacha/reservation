cafe6e33cb05b2a91f73b579ac694c1b5361febf
luckychacha <358140284@qq.com>
2023-02-12 01:16:00 +0800

    day 32: Rewrite tests in reservation-service.

--- service/src/service.rs
+++ service/src/service.rs
@@ -86,25 +86,47 @@ impl ReservationService for RsvpService {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Deref;
+
     use super::*;
     use luckychacha_reservation_abi::Reservation;
 
     use luckychacha_sqlx_pg_tester::TestDb;
 
+    pub struct TestConfig {
+        #[allow(dead_code)]
+        tdb: TestDb,
+        pub config: Config,
+    }
+
+    impl TestConfig {
+        pub fn new() -> Self {
+            let mut config = Config::load("fixtures/config.yml").unwrap();
+
+            let tdb = TestDb::new(
+                &config.db.user,
+                &config.db.password,
+                &config.db.host,
+                config.db.port,
+                "../migrations",
+            );
+
+            config.db.dbname = tdb.dbname.clone();
+            Self { tdb, config }
+        }
+    }
+
+    impl Deref for TestConfig {
+        type Target = Config;
+
+        fn deref(&self) -> &Self::Target {
+            &self.config
+        }
+    }
+
     #[tokio::test]
     async fn reserve_should_work() {
-        let mut config = Config::load("fixtures/config.yml").unwrap();
-
-        let tdb = TestDb::new(
-            &config.db.user,
-            &config.db.password,
-            &config.db.host,
-            config.db.port,
-            "../migrations",
-        );
-
-        config.db.dbname = tdb.dbname.clone();
-
+        let config = TestConfig::new();
         let service = RsvpService::from_config(&config).await.unwrap();
 
         let reservation: Reservation = Reservation::new_pending(
