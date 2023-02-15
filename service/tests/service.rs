deb87c1aee12459651cf91ff6ed3ed0db2ed7005
luckychacha <358140284@qq.com>
2023-02-15 18:31:17 +0800

    day 35: Add tests directory in service module.

--- service/tests/service.rs
+++ service/tests/service.rs
@@ -1,2 +1,10 @@
+use luckychacha_reservation_abi::Config;
+use luckychacha_reservation_service::start_server;
+
 #[tokio::test]
-async fn grpc_server_should_work() {}
+async fn grpc_server_should_work() {
+    tokio::spawn(async move {
+        let config = Config::load("../fixture/config.yml").unwrap();
+        start_server(&config).await.unwrap();
+    });
+}
