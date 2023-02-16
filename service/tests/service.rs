2d85c648727a62f8e4af72e8f9fea9aec1124da4
luckychacha <358140284@qq.com>
2023-02-16 18:35:31 +0800

    day 36: Integration testing: Part 1: Add dbtester dependency and Reserve grpc test.

--- service/tests/service.rs
+++ service/tests/service.rs
@@ -25,13 +25,22 @@ async fn grpc_server_should_work() {
         .await
         .unwrap();
 
-    let rsvp = Reservation::new_pending(
+    let mut rsvp = Reservation::new_pending(
         "luckychacha-id",
         "ocean-view-room-666",
         "2022-12-25T15:00:00+0800".parse().unwrap(),
         "2022-12-28T11:00:00+0800".parse().unwrap(),
         String::from("I'll arrive at 3pm. Please help to upgrade to execuitive room if possible."),
     );
-    let request = ReserveRequest::new(rsvp);
-    let _ = client.reserve(request).await;
+    let request = ReserveRequest::new(rsvp.clone());
+    let ret: Reservation = client
+        .reserve(request)
+        .await
+        .unwrap()
+        .into_inner()
+        .reservation
+        .unwrap();
+
+    rsvp.id = ret.id;
+    assert_eq!(ret, rsvp);
 }
